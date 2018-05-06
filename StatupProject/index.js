131e1e3f57a4a09484cf9e5b202a3e0edb3d9762
abhishek sharma <abhishek.sharma@ATMECS.CORP>
2018-05-06 23:42:26 +0530

    latest code changes

--- StatupProject/index.js
+++ StatupProject/index.js
@@ -1,13 +1,20 @@
-//calling the express package
+//importing the express package
 const express=require('express');
-
+//importing the passport and passport-goole-oauth2.0 library for google authentication
+const passport=require('passport');
+const google_authentication=require('passport-goole-oauth20').strategy;
 const app=express(); // in app object we have all the features of express
 
+passport.use(new google_authentication());//passsort don't which strategy need to use for app so by this loc we are telling.
+
+
+
+
 // route handler with error function(which have request and response object)
-app.get('/', (req,res)=>
+/*app.get('/', (req,res)=>
 {
   res.send({ Name:'Abhishek Sharma',Gender:'Male',Title:'Software Engineer'});
-});
+});*/
 
 //now if you want to deploy your app(code) into server(Heroku(currentky for this app we are using),AWS,MICROSOFT AZURE)
 // to listen heroku port
